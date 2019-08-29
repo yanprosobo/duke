9365e7f6c22006ec64273ee145ceefbdd2e4f539
yanprosobo <peiyanbo@hotmail.com>
2019-08-29 15:30:31 +0800

    Implemented Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,20 @@
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Duke {
+    private static List<String> list1 = new ArrayList<>();
+
+    private static void printList (List<String> list1) {
+        printIndentedHorizontalLine();
+        int i = 0;
+        for (String temp: list1) {
+            printIndentMessage(i+1 + ". " + temp);
+            i++;
+        }
+
+        printIndentedHorizontalLine();
+    }
     private static void printIndentMessage (String message) {
         System.out.println("\t" + message);
     }
@@ -20,21 +34,26 @@ public class Duke {
     }
     private static void printReply(String reply) {
         printIndentedHorizontalLine();
-        printIndentMessage(reply);
+        printIndentMessage("added: " + reply);
         printIndentedHorizontalLine();
     }
     private static void reply() {
-        String terminate = "bye";
+        String terminateCommand = "bye";
+        String listCommand = "list";
 
         Scanner input = new Scanner(System.in);
         String commands =  input.nextLine();
 
-        if(commands.equals(terminate)) {
+        if(commands.equals(terminateCommand)) {
             printGoodbye();
             java.lang.System.exit(0);
             return;
         }
+        else if (commands.equals(listCommand)){
+            printList(list1);
+        }
         else {
+            list1.add(commands);
             printReply(commands);
         }
     }
@@ -45,7 +64,6 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);*/
-
         printWelcome();
         while (true) {
             reply();
