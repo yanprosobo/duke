418a3e9da762eae0479ef5aca261b535adde14ab
yanprosobo <peiyanbo@hotmail.com>
2019-09-12 18:23:12 +0800

    Added Javadocs for todo.java

--- src/main/java/Todo.java
+++ src/main/java/Todo.java
@@ -2,7 +2,12 @@ import java.util.List;
 
 public class Todo extends Task {
     private String description;
-
+    /**
+     * Creates a todo task submitted by user
+     *
+     * @param input user input as prompted
+     * @throws DukeException error message if user input was invalid
+     */
     public Todo(List<String> list1) throws DukeException{
         if (list1.size() == 0) {
             throw new DukeException("Please enter valid format: todo <taskname>");
